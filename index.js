95d48adf7e04a08af86130ff3906e61dd8ba181c
Brian Hudson <brianjasonhudson@gmail.com>
2017-07-24 12:41:58 -0500

    Connected db and created endpoints

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 // Grab environment variables from .env file
 require('dotenv').config()
 
-// Set up server
+// Require packages
 const express = require('express')
+const {MongoClient} = require('mongodb')
 
+// Set up server
 const app = express()
+
+// Set up db
+MongoClient.connect(process.env.DATABASE_URL)
+.then(db => {
+   console.log(db)
+   app.set('db', db)
+})
+
+app.get('/api/users', (req, res, next) => {
+   const db = req.app.get('db')
+   const User = db.collection('users')
+   User.find({name: "Brian"}).toArray()
+   .then(result => {
+      console.log(result)
+      res.send(result)
+   })
+   .catch(err => {
+      res.send(err)
+   })
+})
+app.post('/api/users', (req, res, next) => {
+   const db = req.app.get('db')
+   const User = db.collection('users')
+   User.insertOne({name: "Brian"})
+   .then(result => {
+      console.log(result)
+      res.send(result)
+   })
+   .catch(err => {
+      res.send(err)
+   })
+})
 
 app.listen(process.env.PORT, () => {
    console.log(`Listening on port ${process.env.PORT}`)
